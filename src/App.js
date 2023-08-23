2a29a3e378b9a5d63fab9bd913a724c9fc42ac02
numrahtbox <numrah.tbox@gmail.com>
2023-08-23 06:37:21 -0400

    Revert "my comint"
    This reverts commit ef3e994eb543fba64940989de023ce700c044310.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import About from './pages/About';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import User from './pages/User';
-//this 
 //  this is new
 function App() {
   return (
